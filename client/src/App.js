b97293fbc458fc1463e93be0bde3e9882ce60f3c
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-04-29 09:59:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import QueryProductFeatures from './components/QueryProductFeatures';
 import QueryCompanyOutlets from './components/QueryCompanyOutlets';
 import PricingDetails from './components/PricingDetails';
 import ComplaintForm from './components/ComplaintForm';
+import Complaints from './Complaints';
+import LoginPage from './LoginPage';
+import Outlets from './Outlets';
+import Products from './Products';
+import Dashboard from './Dashboard';
 function App() {
   return (
     <Router>
@@ -17,6 +22,11 @@ function App() {
         <Route path="/price" element={<PricingDetails />} />
         <Route path="/complaints_form" element={<ComplaintForm />} />
         <Route path="/complaints_status" element={<ComplaintStatus />} />
+        <Route path="/admin_products" element={<Products/>} />
+        <Route path="/admin_outlets" element={<Outlets />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin_complaints" element={<Complaints />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
